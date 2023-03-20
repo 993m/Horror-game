f401335a39eff63b91d30e5cee6323672ec0cac6
993m <cioclov_maria@yahoo.com>
2023-03-20 14:59:23 +0200

    Initial terrain + player movement

---
+++ horror game/Assets/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 40f;
+
+    float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // blocheaza cursorul in centrul ecranului si il si ascunde
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        // rotatia mouse sus-jos
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // rotatia mouse stanga-dreapta
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
