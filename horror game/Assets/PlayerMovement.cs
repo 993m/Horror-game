5eadee03569ff2f828773c7736a44fac08fabc68
criss505 <100491898+criss505@users.noreply.github.com>
2023-04-20 19:49:52 +0300

    Add files via upload
    shift sprint

--- horror game/Assets/PlayerMovement.cs
+++ horror game/Assets/PlayerMovement.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12f;
+    public float defaultSpeed = 4f;
+    public float sprintSpeed = 10f;
+    public float speed;
+    public float sprintTime = 10.0f; // total time for sprint
+    private float sprintTimer; // current time for sprint
 
     public float gravity = -9.81f; 
     Vector3 velocity;
@@ -17,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = defaultSpeed;
     }
 
     // Update is called once per frame
@@ -29,6 +33,20 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
+        // daca playerul apasa pe shift alearga
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0)
+        {
+            speed = sprintSpeed; // set current speed to sprint speed
+            sprintTimer -= Time.deltaTime; // decrease the sprint timer
+        }
+        else
+        {
+            speed = defaultSpeed; // set current speed to default speed
+            if (sprintTimer < sprintTime)
+            {
+                sprintTimer += Time.deltaTime; // increase the sprint timer if it's not already at max
+            }
+        }
 
         // miscare fata spate stanga dreapta
         float x = Input.GetAxis("Horizontal");
@@ -41,6 +59,7 @@ public class PlayerMovement : MonoBehaviour
         // gravitatie
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
 
     }
 }
