0f3313e71291b801865366b23f95a35cc5a9eef4
993m <cioclov_maria@yahoo.com>
2023-05-11 20:37:46 +0300

    Revert "Revert "Map borders""
    This reverts commit 65c4a874357f3ea635aacd18f61cf59d8d3426a0.

--- horror game/Assets/PlayerMovement.cs
+++ horror game/Assets/PlayerMovement.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12f;
+    public float speed;
 
-    public float gravity = -9.81f; 
+    public float defaultSpeed = 4f;
+    public float sprintSpeed = 10f;
+    public float sprintTime = 10.0f; // total time for sprint
+    private float sprintTimer; // current time for sprint
+
+    public float gravity = -9.81f;
     Vector3 velocity;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,24 +29,53 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
+        Gravity();
 
+        Sprint();
 
+        Move();
+    }
+
+    void Move()
+    {
         // miscare fata spate stanga dreapta
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+    }
 
+    void Sprint()
+    {
+        // daca playerul apasa pe shift alearga
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0)
+        {
+            speed = sprintSpeed; // set current speed to sprint speed
+            sprintTimer -= Time.deltaTime; // decrease the sprint timer
+        }
+        else
+        {
+            speed = defaultSpeed; // set current speed to default speed
+            if (sprintTimer < sprintTime && !Input.GetKey(KeyCode.LeftShift))
+            {
+                sprintTimer += Time.deltaTime; // increase the sprint timer if it's not already at max
+            }
+        }
+    }
+
+    void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
         // gravitatie
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
     }
+
+    
 }
